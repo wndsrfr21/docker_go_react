bebfdc06b8a3f64519e0c9b7c98f63f4d14a6a42
Alex Donn <wndsrfr21@gmail.com>
2022-02-04 18:52:56 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  // Creates default gin router with Logger and Recovery middleware already attached
+  router := gin.Default()
+
+  // Create API route group
+  api := router.Group("/api")
+  {
+    // Add /hello GET route to router and define route handler function
+    api.GET("/hello", func(ctx *gin.Context) {
+      ctx.JSON(200, gin.H{"msg": "world"})
+    })
+  }
+
+  router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
+
+  // Start listening and serving requests
+  router.Run(":8080")
+}
